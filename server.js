dfbdb7c8632889a8d7d2590244e7e9dd360df040
Ivy Bean <ivieking@gmail.com>
2020-09-24 09:57:32 -0400

    added server.js and get request

--- server.js
+++ server.js
@@ -1,42 +1,14 @@
 // Dependencies
-// =============================================================
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const notesJSON = require("/db.json");
 
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3000;
+// Server Setup
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-// Sets up the Express app to handle data parsing
+//Express
+app.use(express.static('/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-
-// Sets up Routes
-// =============================================================
-
-//Notes in JSON
-app.get("/api/notes", function(req, res) {
-  return res.json(notesJSON);
-})
-
-
-//HTML Routes
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-});
-
-
-// Server listening to PORT
-// =============================================================
-app.listen(PORT, function() {
-  console.log("App is listening on PORT " + PORT)
-});
